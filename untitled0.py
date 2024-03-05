309969a188edcd167fb047ff77463395d321b099
TequilaSunsets <kevin.trejo15052001@gmail.com>
2024-03-05 11:16:46 -0600

    creacion-archivo
    Se creo el archivo de tipo .py para el desarrollo del programa que analice expresiones regulares.

---
+++ untitled0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 11:12:34 2024
+
+@author: naked
+"""
+
+#Codigo para el analizador de expresiones regulares#
+
+#Sugiero que no se acepte la operacion de cerradura positiva, para simplificar
+#las expresiones regulares.
+
+ER = ["(0+1)*","0(11)*0"]
+
+class Analizador:
+    
